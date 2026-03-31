7aefa9ec97beb1e05ce351b31fa93180a8cc24bb
Nitesh Pandey <niteshpandey8969@gmail.com>
2026-03-31 13:15:36 +0530

    Database

---
+++ Database/src/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+const notes = []
+app.get("/",(req,res)=>{
+    res.send("Namaste Duniya ")
+
+})
+app.post('/notes',(req,res)=>{
+notes.push(req.body)
+res.status(201).json({
+    message : "notes created successfully "
+})
+})
+
+
+app.get('/notes',(req,res)=>{
+res.status(200).json(
+    {
+        notes:notes
+    }
+)
+})
+
+app.delete('/notes/:index',(req,res)=>{
+    delete notes[req.params.index]
+    res.status(204).json({
+        message:"Notes  deleted successfully "
+    })
+})
+
+app.patch('/notes/:index',(req,res)=>{
+    notes[req.params.index].Description = req.body.Description
+    res.status(200).json({
+        message:"Notes Updated Successfully"
+    }) 
+})
+module.exports= app
