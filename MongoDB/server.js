495e254be1a24cfe2309f6a615059c0901f61fd9
Nitesh Pandey <niteshpandey8969@gmail.com>
2026-04-01 14:10:00 +0530

    MongoDB

--- MongoDB/server.js
+++ MongoDB/server.js
@@ -1,17 +1,14 @@
 const app = require('./src/app')
+const mongoose = require('mongoose')
 
-const mongoose =  require('mongoose')
 
-
- function connectToDb() {
-     mongoose.connect("mongodb+srv://lexarCoder:KKMbxeis5WLgB9dF@cluster0.sadubrt.mongodb.net/MongoDB")
-     .then(()=>{
-        console.log("Conneted to database");
+function connectDB() {
+    mongoose.connect('')
+    .then(()=>{
+        console.log("Database connected...");
         
-     })
- }
-
- connectToDb()
+    })
+}
 
 app.listen(3000,()=>{
     console.log("Port runnnig on 3000");
