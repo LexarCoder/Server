6c8f3cf16e484e053e7538c5cafb41d579ef4194
Nitesh Pandey <niteshpandey8969@gmail.com>
2026-04-03 14:21:32 +0530

    CURD Done

--- CURD/src/app.js
+++ CURD/src/app.js
@@ -9,21 +9,32 @@ app.get('/', (req, res) => {
     res.send("Good Morning Gyes")
 })
 
+
+//  Notes Create 
+
 app.post('/notes', async (req,res)=>{
       const {title, description} = req.body
    const notes =  await noteModel.create({
     title, description
 })
-
-aap.get('/notes',(erq,res)=>{
-    
-})
-
 res.status(201).json({
     message:"Successfully created ...",
     notes
 })
 })
+
+//  Notes Data get 
+
+app.get('/notes', async (erq, res) => {
+    const note = await noteModel.find()
+    res.status(200).json({
+        message: "Notes fetch successfully...",
+         note
+    })
+})
+
+//  Notes Data patch
+//  Notes Data Deleted 
 
 module.exports= app
 
